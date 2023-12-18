1091d141efcc39679c5dce945b4135baacd29e5d
Amodh2022 <103436151+Amodh2022@users.noreply.github.com>
2023-12-18 22:19:32 +0530

    Add files via upload

--- app/profile/personaverification/page.tsx
+++ app/profile/personaverification/page.tsx
@@ -1,51 +1,42 @@
-'use client';
-import React, { useEffect } from 'react';
-import { createBrowserHistory } from 'history';// Import useHistory from React Router
-import Persona from 'persona';
-import { callApi } from '../../_actions/personaupdateapi';
-import { useRouter } from 'next/navigation';
+'use client'
 
+import React, { useEffect, useState } from 'react';
+import dynamic from 'next/dynamic'; // Import dynamic from next/dynamic
+import { callApi } from '../../_actions/personaupdateapi';
+import { useRouter } from 'next/router'; // Import useRouter from 'next/router'
+// import Persona from 'persona' // Use dynamic import for Persona
 
 const InlineInquiry = () => {
-    const router = useRouter();
-    var myuserId;
+  const router = useRouter();
+  const [myuserId, setMyuserId] = useState<string | null>(null);
 
-    useEffect(() => {
+  useEffect(() => {
+    // Check if window is defined before using localStorage
+    if (typeof window !== 'undefined') {
+      const userIdFromStorage = localStorage.getItem('userId');
+      if (userIdFromStorage) {
+        setMyuserId(userIdFromStorage);
+      }
+    }
+  }, []);
+
+  const handleComplete = async ({ inquiryId, status, fields }: any) => {
+    try {
+      if (status === 'completed') {
+        // await callApi(inquiryId, myuserId);
+        // router.push('/profile');
+      }
+    } catch (e) {
+      console.error("Error in handleComplete:", e);
+      throw Error(e);
+    }
+  };
+
+  return (
+    <div className='h-screen flex justify-center'>
       
-        myuserId = localStorage.getItem('userId');
-       
-  
-    }, []);
-
-    const divStyle = {
-        width: '100%', 
-    };
-
-    const handleComplete = async ({ inquiryId, status, fields }) => {
-        try {
-            if (status === 'completed') {
-                await callApi(inquiryId, myuserId);
-                router.push('/profile'); 
-              
-            }
-        } catch (e) {
-            console.error("Error in handleComplete:", e);
-            throw Error(e);
-        }
-    };
-
-    return (
-        <div className='h-screen flex justify-center' style={divStyle}>
-            <Persona.Inquiry
-                templateId='itmpl_oFwr5vDFxPnJVnpKmXpgxY5x'
-                environmentId='env_3gPXHtfowwicvW8eh5GdW9PV'
-                onLoad={() => {
-                    console.log('Loaded inline');
-                }}
-                onComplete={handleComplete}
-            />
-        </div>
-    );
+    </div>
+  );
 };
 
 export default InlineInquiry;
