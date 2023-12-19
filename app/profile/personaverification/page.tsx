89b5d0f4bb933c0fc632a7dbaf35e50b66f4f64a
Amodh2022 <103436151+Amodh2022@users.noreply.github.com>
2023-12-19 22:49:04 +0530

    Add files via upload

--- app/profile/personaverification/page.tsx
+++ app/profile/personaverification/page.tsx
@@ -1,46 +1,47 @@
-'use client'
-
-import React, { useEffect, useState } from 'react';
-import dynamic from 'next/dynamic'; // Import dynamic from next/dynamic
-import { callApi } from '../../_actions/personaupdateapi';
-// import { useRouter } from 'next/router'; // Import useRouter from 'next/router'
-import Persona from 'persona' // Use dynamic import for Persona
-
-const InlineInquiry = () => {
+"use client"
  
-  const [myuserId, setMyuserId] = useState<string | null>(null);
-
-  useEffect(() => {
-    // Check if window is defined before using localStorage
-    if (typeof window !== 'undefined') {
-      const userIdFromStorage = localStorage.getItem('userId');
-      if (userIdFromStorage) {
-        setMyuserId(userIdFromStorage);
-      }
-    }
-  }, []);
-
-  const handleComplete = async ({ inquiryId, status, fields }: any) => {
-    try {
-      if (status === 'completed') {
-        await callApi(inquiryId, myuserId);
-        // router.push('/profile');
-      }
-    } catch (e) {
-      console.error("Error in handleComplete:", e);
-      throw Error(e);
-    }
-  };
-
-  return (
-    <div className='h-screen flex justify-center'>
-      <Persona.Inquiry
-        templateId='itmpl_oFwr5vDFxPnJVnpKmXpgxY5x'
-        environmentId='env_3gPXHtfowwicvW8eh5GdW9PV'
-        onComplete={handleComplete}
-      />
-    </div>
-  );
+import React, { useEffect, useState } from 'react';
+import dynamic from 'next/dynamic';
+import { callApi } from '../../_actions/personaupdateapi';
+import { useRouter } from 'next/router';
+ 
+// Dynamically import the Persona component with SSR disabled
+const PersonaInquiry = dynamic(
+    () => import('persona').then((mod) => mod.Inquiry),
+    { ssr: false }
+);
+ 
+const InlineInquiry = () => {
+    const [userID, setUserID] = useState('');
+ const router=useRouter()
+    useEffect(() => {
+        const storedUserID = localStorage.getItem('userId');
+        if (storedUserID) {
+            setUserID(storedUserID);
+        }
+    }, []);
+ 
+    const handleComplete = async ({ inquiryId, status }) => {
+        if (status === 'completed') {
+            try {
+                await callApi(inquiryId, userID);
+                router.push('/profile')
+            } catch (e) {
+                console.error("Error in handleComplete:", e);
+            }
+        }
+    };
+ 
+    return (
+        <div className='h-screen flex justify-center' style={{ width: '100%' }}>
+            <PersonaInquiry
+                templateId='itmpl_oFwr5vDFxPnJVnpKmXpgxY5x'
+                environmentId='env_3gPXHtfowwicvW8eh5GdW9PV'
+                onLoad={() => console.log('Loaded inline')}
+                onComplete={handleComplete}
+            />
+        </div>
+    );
 };
-
+ 
 export default InlineInquiry;
